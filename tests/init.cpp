a1653d44214930dd06b4d7bae3a7faf783ef8aef
IvanovMA <win77789@gmail.com>
2016-11-06 15:14:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -9,6 +9,13 @@ SCENARIO("count", "[count]"){
   REQUIRE(s.count()==1);
 }
 
+SCENARIO("push", "[push]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
+}
+
 SCENARIO("pop", "[pop]"){
   stack<int> s;
   s.push(1);
@@ -16,3 +23,32 @@ SCENARIO("pop", "[pop]"){
   REQUIRE(s.count()==0);
 }
 
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2;
+  s2=s;
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
+}
+
+SCENARIO("cop", "[cop]"){
+   stack<int> s;
+   s.push(1);
+   stack<int> s2=s;
+   REQUIRE(s2.count()==1);
+   REQUIRE(s2.top()==1);
+ }
+
+SCENARIO("top", "[top]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.top()==1);
+}
+
+SCENARIO("empty", "[empty]"){
+  stack<int> s1, s2;
+  s1.push(1);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
+}
