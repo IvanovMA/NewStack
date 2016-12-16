f5fc2b4a45c1cae4c5bfe790362936dfb719aa76
IvanovMA <win77789@gmail.com>
2016-12-16 15:47:23 +0400

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -186,7 +186,7 @@ auto allocator<T>::refact()->void
 	size_t j=0;
 	for (size_t i=0;i<size_; i++)
 	{
-		if(bitset<T>::ptr_) 
+		if(bitset::ptr_) 
 		{
 			buff.construct(buff.ptr_ + j, ptr_[i]);
 			++j;
